b5afa5c5f5225abc346812d51105d8cdf72660bc
Luis Rachope <luis.chiasso@hotmail.com>
2023-02-07 21:26:23 -0300

    Ajuste no layout com annotation para trazer o nome formatado corretamente.

--- BusinessPaymentsWebApp/Models/ViewModels/Dashboard.cs
+++ BusinessPaymentsWebApp/Models/ViewModels/Dashboard.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,16 @@ namespace BusinessPaymentsWebApp.Models.ViewModels
 {
     public class Dashboard
     {
+        [Display(Name = "Total Clients")]
         public int TotalClients { get; set; }
+
+        [Display(Name = "Total Credits")]
         public int TotalCredits { get; set; }
+
+        [Display(Name = "Total Recovery")]
         public int TotalRecovery { get; set; }
+
+        [Display(Name = "Total Peding Amounts")]
         public int TotalPedingAmounts { get; set; }
     }
 }
